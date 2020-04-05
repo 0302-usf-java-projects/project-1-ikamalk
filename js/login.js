0ca80094356034c9a60786d4085517ae60f91171
Kamal <kamalaitsidhoum@gmail.com>
2020-04-04 20:16:42 -0400

    form disabled when we sumbit

--- js/login.js
+++ js/login.js
@@ -8,6 +8,7 @@ var loginButton = document.getElementById("login-submit-btn");
 var registerButton = document.getElementById("register-submit-btn");
 
 
+
 function goToCreateAccount() {
     loginContainer.style.display = "none";
     RegisterContainer.style.display = "block"
@@ -20,7 +21,13 @@ function goToLogin() {
 }
 
 function login() {
+    //form login
+    var loginUsername = document.getElementById('login-username');
+    var loginPassword = document.getElementById('login-password');
+
     //ui
+    loginUsername.disabled = true;
+    loginPassword.disabled = true;
     loginButton.disabled = true;
     loginButton.innerHTML = `<div class="spinner-container"><div class="spinner">
     <div></div><div><div></div></div>
@@ -29,13 +36,27 @@ function login() {
     //emulate
     setTimeout(() => {
         //ui
+        loginUsername.disabled = false;
+        loginPassword.disabled = false;
         loginButton.disabled = false;
     loginButton.innerHTML = `Login`;
     }, 2000);
 }
 
 function register() {
+    //form register
+    var registerUsername = document.getElementById("register-username");
+    var registerPassword = document.getElementById("register-password");
+    var registerFirstName = document.getElementById("register-first-name");
+    var registerLastName = document.getElementById("register-last-name");
+    var registerEmail = document.getElementById("register-email");
+
      //ui
+     registerUsername.disabled = true;
+     registerPassword.disabled = true;
+     registerFirstName.disabled = true;
+     registerLastName.disabled = true;
+     registerEmail.disabled = true;
      registerButton.disabled = true;
      registerButton.innerHTML = `<div class="spinner-container"><div class="spinner">
      <div></div><div><div></div></div>
@@ -44,6 +65,11 @@ function register() {
      //emulate
      setTimeout(() => {
          //ui
+         registerUsername.disabled = false;
+         registerPassword.disabled = false;
+         registerFirstName.disabled = false;
+         registerLastName.disabled = false;
+         registerEmail.disabled = false;
          registerButton.disabled = false;
          registerButton.innerHTML = `Register`;
      }, 2000);
