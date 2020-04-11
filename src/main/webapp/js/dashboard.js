a034fd8b5ce812cbcefc4fc17aaf4ad979d1015e
Kamal <kamalaitsidhoum@gmail.com>
2020-04-11 17:05:25 -0400

    add waiting spinner + fixed some design

--- src/main/webapp/js/dashboard.js
+++ src/main/webapp/js/dashboard.js
@@ -1,42 +1,13 @@
 saidWelcome();
 
-
-
-$(document).ready(function() {
-    $('#myTableReimbursement').DataTable();
-} );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function () {
+  $("#myTableReimbursement").DataTable();
+});
 
 function saidWelcome() {
-    new Typed('#navbar-title', {
-        strings: ["", "Welcome to the FakeCompany !"],
-        typeSpeed: 25,
-        showCursor: false
-      });
-
-    }
+  new Typed("#navbar-title", {
+    strings: ["", "Welcome to the FakeCompany !"],
+    typeSpeed: 25,
+    showCursor: false,
+  });
+}
